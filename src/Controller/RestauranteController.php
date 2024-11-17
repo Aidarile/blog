48fab785a736a41ce20f300deb9275c8fd03c96a
Aida Garmer <aidarile@gmail.com>
2024-11-17 20:42:58 +0100

    ultima actualizacion de visitas, estilos y varios

--- src/Controller/RestauranteController.php
+++ src/Controller/RestauranteController.php
@@ -10,129 +10,172 @@ use App\Entity\Restaurante;
 use App\Repository\RestauranteRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\JsonResponse;
-
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Validator\Constraints\NotBlank;
-use Symfony\Component\Validator\Constraints\Type;
-use Symfony\Component\Validator\Mapping\ClassMetadata;
 use Symfony\Component\Validator\Constraints\Length;
-
 
 class RestauranteController extends AbstractController
 {
     #[Route('/restaurante', name: 'listado_restaurantes', methods: ["GET"])]
-    public function listaRestaurantes(RestauranteRepository $repo): Response {
+    public function listaRestaurantes(RestauranteRepository $repo): Response 
+    {
+        $listadoRestaurantes = $repo->findAll();
 
-        $listadoRestaurantes = $repo -> findAll();
-
-        return $this -> render('restaurante/index.html.twig', ["controller_name" => "Esto muestra todos los restaurantes", 
-        "listadoRestaurantes" => $listadoRestaurantes]);    
+        return $this->render('restaurante/index.html.twig', [
+            "controller_name" => "Esto muestra todos los restaurantes", 
+            "listadoRestaurantes" => $listadoRestaurantes
+        ]);
     }
 
     #[Route('/restaurante/form', name: 'crear_restaurante', methods: ["GET", "POST"])]
-    public function crearRestaurante(EntityManagerInterface $emi, Request $request): Response {
-
+    public function crearRestaurante(EntityManagerInterface $emi, Request $request): Response 
+    {
         $restaurante = new Restaurante();
 
-        $fb = $this -> createFormBuilder($restaurante);
+        $fb = $this->createFormBuilder($restaurante)
+            ->add("Nombre", TextType::class, [
+                "constraints" => [
+                    new Length(["min" => 1, "max" => 256]),
+                    new NotBlank()
+                ],
+                'attr' => [
+                    'placeholder' => 'Nombre del Restaurante'
+                ]
+            ])
+            ->add("Direccion", TextType::class, [
+                "constraints" => [
+                    new Length(["min" => 1, "max" => 256]),
+                    new NotBlank()
+                ],
+                'attr' => [
+                    'placeholder' => 'Direccion del Restaurante'
+                ]
+            ])
+            ->add("Telefono", TextType::class, [
+                "required" => false,
+                "constraints" => [
+                    new Length(["min" => 9, "max" => 12])
+                ],
+                'attr' => [
+                    'placeholder' => 'Telefono del Restaurante'
+                ]
+            ])
+            ->add("Tipo_de_cocina", TextType::class, [
+                "required" => false,
+                "constraints" => [
+                    new Length(["min" => 1, "max" => 256])
+                ],
+                'attr' => [
+                    'placeholder' => 'Tipo de cocina del Restaurante'
+                ]
+            ])
+            ->add("GUARDAR", SubmitType::class);
 
-        $fb->add("Nombre", TextType::class, [
+        $formulario = $fb->getForm();
+        $formulario->handleRequest($request);
 
-            "constraints"=>[
-                new Length(["min"=>1,"max"=> 256]),
-                new NotBlank()
-            ]
+        if ($formulario->isSubmitted() && $formulario->isValid()) {
+            $restaurante = $formulario->getData();
+            $emi->persist($restaurante);
+            $emi->flush();
+
+            return $this->redirectToRoute("listado_restaurantes");
+        }
+
+        return $this->render("restaurante/crearRestaurante.html.twig", [
+            "formulario" => $formulario
         ]);
-        $fb->add("Direccion", TextType::class, [
+    }
 
-            "constraints"=>[
-                new Length(["min"=>1,"max"=> 256]),
-                new NotBlank()
-            ]
+    #[ Route("/restaurante/{idRestaurante}", name: 'mostrar_restaurante', methods: ["GET"])]
+    public function mostrarRestaurante (RestauranteRepository $repo, int $idRestaurante): Response{
+
+	$restaurante = $repo->find($idRestaurante);
+
+
+    return $this->render('restaurante/mostrarRestaurante.html.twig', [
+        "controller_name" => "Detalles del Restaurante seleccionado",
+         "restaurante" => $restaurante
         ]);
-        $fb->add("Telefono", TextType::class, [
-            "required" => false,
-            "constraints"=>[
-                new Length(["min"=>1,"max"=> 12])
-            ]
+	}
+
+    #[Route('/restaurante/form/{idRestaurante}', name: 'actualizar_restaurante', methods: ["GET", "POST"])]
+    public function actualizarRestaurante(RestauranteRepository $repo, EntityManagerInterface $emi, int $idRestaurante, Request $request): Response 
+    {
+        $restaurante = $repo->find($idRestaurante);
+
+        $fb = $this->createFormBuilder($restaurante)
+            ->add('Nombre', TextType::class, [
+                "constraints" => [
+                    new Length(["min" => 1, "max" => 256]),
+                    new NotBlank()
+                ],
+                'attr' => [
+                    'placeholder' => 'Nombre del Restaurante'
+                ]
+            ])
+            ->add("Direccion", TextType::class, [
+                "constraints" => [
+                    new Length(["min" => 1, "max" => 256]),
+                    new NotBlank()
+                ],
+                'attr' => [
+                    'placeholder' => 'Dirección del Restaurante'
+                ]
+            ])
+            ->add("Telefono", TextType::class, [
+                "required" => false,
+                "constraints" => [
+                    new Length(["min" => 9, "max" => 12])
+                ],
+                'attr' => [
+                    'placeholder' => 'Telefono del restaurante'
+                ]
+            ])
+            ->add("Tipo_de_cocina", TextType::class, [
+                "required" => false,
+                "constraints" => [
+                    new Length(["min" => 1, "max" => 256])
+                ],
+                'attr' => [
+                    'placeholder' => 'Tipo de cocina del Restaurante'
+                ]
+            ])
+            ->add('Guardar', SubmitType::class, [
+                'label' => 'GUARDAR',
+            ]);
+
+        $formulario = $fb->getForm();
+        $formulario->handleRequest($request);
+
+        if ($formulario->isSubmitted() && $formulario->isValid()) {
+            $emi->flush();
+
+            return $this->redirectToRoute("listado_restaurantes");
+        }
+
+        return $this->render("restaurante/actualizarRestaurante.html.twig", [
+            "formulario" => $formulario
         ]);
-        $fb->add("Tipo_de_cocina", TextType::class, [
-            "required" => false,
-            "constraints"=>[
-                new Length(["min"=>1,"max"=> 256])
-            ]
-        ]);
+    }
 
-        $fb->add("Guardar", SubmitType::class);
+    #[Route('/restaurante/{idRestaurante}', name: 'eliminar_restaurante', methods: ["POST"])]
+public function eliminarRestaurante(RestauranteRepository $repo, EntityManagerInterface $emi, int $idRestaurante): Response 
+{
+    $restaurante = $repo->find($idRestaurante);
 
-        $formulario = $fb -> getForm();
+    if ($restaurante) {
+        if (empty($restaurante->getVisitas()[0])) {
+            $emi->remove($restaurante);
+            $emi->flush();
 
-        $formulario -> handleRequest($request);
-
-        if ($formulario -> isSubmitted() && $formulario -> isValid()){
-            $restaurante = $formulario -> getData();
-
-            $emi -> persist ($restaurante);
-            $emi -> flush();
-            
-        return  $this -> redirectToRoute("listado_restaurantes");
+            $this->addFlash('success', 'Restaurante eliminado con éxito.');
         } else {
-        return $this -> render("restaurante/crearRestaurante.html.twig", ["formulario" => $formulario]);
+            $this->addFlash('error', 'No puedes eliminar un restaurante con visitas activas, elimina las visitas primero.');
         }
-
-       /* $restaurante -> setNombre("Burguer Lhorta");
-        $restaurante -> setDireccion("Avenida Grande 32, Albal");
-        $restaurante -> setTelefono("961234567");
-        $restaurante -> setTipoDeCocina("Hamburgueseria");
-
-        $emi -> persist ($restaurante);
-        $emi -> flush();
-
-        return new JsonResponse("El restaurante " .$restaurante->getNombre() . " con ID " . $restaurante-> getId() . " ha sido creado", Response::HTTP_CREATED);
-        // RESPUESTA MÁS SENCILLA QUE LA DE ARRIBA: return new JsonResponse("El restaurante introducido ha sido creado");
-        */
-        }
-
-
-
-    #[Route('/restaurante/{idRestaurante}', name: 'mostrar_restaurante', methods: ["GET"])]
-    public function mostrarRestaurante(RestauranteRepository $repo, int $idRestaurante): Response {
-
-        $restaurante = $repo -> find($idRestaurante);
-
-        return $this -> render('restaurante/mostrarRestaurante.html.twig', ["controller_name" => "Este es tu restaurante", 
-        "restaurante" => $restaurante]);    
-    }
-    
-
-   
-    #[Route('/restaurante/{idRestaurante}', name: 'modificar_restaurante', methods: ["PATCH"])]
-    public function modificarRestaurante(RestauranteRepository $repo, EntityManagerInterface $emi, int $idRestaurante): Response {
-        
-        $restaurante = $repo -> find($idRestaurante);
-
-        $restaurante -> setNombre("Bar Italiano");
-        $restaurante -> setDireccion("Calle Dragon Rojo 33");
-        
-        $emi -> flush();
-
-        return new JsonResponse("Dato actualizado", Response::HTTP_OK);
     }
 
-
-    #[Route('/restaurante/{idRestaurante}', name: 'eliminar_restaurante', methods: ["DELETE"])]
-    public function eliminarRestaurante(RestauranteRepository $repo, EntityManagerInterface $emi, int $idRestaurante): Response {
-        
-        $restaurante = $repo -> find($idRestaurante);
-
-        $emi -> remove($restaurante);        
-        $emi -> flush();
-
-        return new JsonResponse("Restaurante eliminado", Response::HTTP_OK);
-    }
-
-
-    
+    return $this->redirectToRoute("listado_restaurantes");
+}
 }
--- src/Controller/VisitaController.php
+++ src/Controller/VisitaController.php
@@ -4,18 +4,203 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
-
+use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Visita;
+use App\Repository\RestauranteRepository;
+use App\Repository\VisitaRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Range;
+use Symfony\Component\Validator\Constraints\Length;
 
 class VisitaController extends AbstractController
 {
-    #[Route('/visita', name: 'app_visita')]
-    public function index(): Response
+    #[Route("/visita/form", name: "crear_visita", methods: ["GET", "POST"])]
+    public function crearVisita(EntityManagerInterface $emi, RestauranteRepository $restauranteRepo, Request $request): Response
     {
-        return $this->render('visita/index.html.twig', [
-            'controller_name' => 'VisitaController',
+        $visita = new Visita();
+
+        $formulario = $this->createFormBuilder($visita)
+            ->add("Restaurante", TextType::class, [
+                "mapped" => false,
+                "constraints" => [
+                    new Length(["min" => 1, "max" => 256]),
+                    new NotBlank()
+                ],
+                "attr" => ["placeholder" => "Nombre del Restaurante"]
+            ])
+            ->add("Valoracion", IntegerType::class, [
+                "constraints" => [
+                    new Range(["min" => 1, "max" => 10]),
+                    new NotBlank()
+                ],
+                "attr" => ["placeholder" => "10"]
+            ])
+            ->add("Comentario", TextType::class, [
+                "constraints" => [
+                    new Length(["min" => 1, "max" => 256]),
+                    new NotBlank()
+                ],
+                "attr" => ["placeholder" => "Escribe tu comentario"]
+            ])
+            ->add("Guardar", SubmitType::class)
+            ->getForm();
+
+        $formulario->handleRequest($request);
+
+        if ($formulario->isSubmitted() && $formulario->isValid()) {
+            // Aquí obtenemos los datos del formulario
+            $restauranteNombre = $formulario->get("Restaurante")->getData();
+            $restaurante = $restauranteRepo->findOneBy(["Nombre" => $restauranteNombre]);
+
+            if ($restaurante !== null) {
+                $visita->setRestaurante($restaurante);
+                $visita->setValoracion($formulario->get("Valoracion")->getData());
+                $visita->setComentario($formulario->get("Comentario")->getData());
+
+                // Persistimos la visita
+                $emi->persist($visita);
+                $emi->flush();
+
+                return $this->redirectToRoute("mostrartodos_visita");
+            } else {
+                $this->addFlash("error", "Debes escribir un Restaurante que exista");
+            }
+        }
+
+        return $this->render("visita/crearVisita.html.twig", [
+            "formulario" => $formulario->createView()
         ]);
+    }
+
+    #[Route("/visita/formRestaurante/{idRestaurante}", name: "crear_visita_con_restaurante", methods: ["GET", "POST"])]
+    public function crearVisitaPorRestaurante(EntityManagerInterface $emi, RestauranteRepository $restauranteRepo, int $idRestaurante, Request $request): Response
+    {
+        $visita = new Visita();
+        $restaurante = $restauranteRepo->find($idRestaurante);
+
+        $formulario = $this->createFormBuilder($visita)
+            ->add("Restaurante", TextType::class, [
+                "mapped" => false,
+                "data" => $restaurante ? $restaurante->getNombre() : '',
+                "constraints" => [
+                    new Length(["min" => 1, "max" => 256]),
+                    new NotBlank()
+                ],
+                "attr" => ["placeholder" => "Nombre del Restaurante"]
+            ])
+            ->add("Valoracion", IntegerType::class, [
+                "constraints" => [
+                    new Range(["min" => 1, "max" => 10]),
+                    new NotBlank()
+                ],
+                "attr" => ["placeholder" => "10"]
+            ])
+            ->add("Comentario", TextType::class, [
+                "constraints" => [
+                    new Length(["min" => 1, "max" => 256]),
+                    new NotBlank()
+                ],
+                "attr" => ["placeholder" => "Escribe tu comentario"]
+            ])
+            ->add("Guardar", SubmitType::class)
+            ->getForm();
+
+        $formulario->handleRequest($request);
+
+        if ($formulario->isSubmitted() && $formulario->isValid()) {
+            $visita->setRestaurante($restaurante);
+            $emi->persist($visita);
+            $emi->flush();
+
+            return $this->redirectToRoute("mostrartodos_visita");
+        }
+
+        $this->addFlash("error", "Debes poner un Restaurante que exista");
+
+        return $this->render("visita/crearVisitaConRestaurante.html.twig", [
+            "formulario" => $formulario->createView()
+        ]);
+    }
+
+    #[Route("/visita", name: "mostrartodos_visita", methods: ["GET"])]
+    public function listaVisitas(VisitaRepository $repo): Response
+    {
+        $visitas = $repo->findAll();
+
+        return $this->render("visita/index.html.twig", [
+            "controller_name" => "Lista de Visitas",
+            "listadoVisitas" => $visitas
+        ]);
+    }
+
+    #[Route("/visita/form/{idVisita}", name: "actualizar_visita", methods: ["GET", "POST"])]
+    public function actualizarVisita(EntityManagerInterface $emi, RestauranteRepository $restauranteRepo, VisitaRepository $repo, int $idVisita, Request $request): Response
+    {
+        $visita = $repo->find($idVisita);
+
+        $formulario = $this->createFormBuilder($visita)
+            ->add("Restaurante", TextType::class, [
+                "mapped" => false,
+                "data" => $visita->getRestaurante() ? $visita->getRestaurante()->getNombre() : '',
+                "constraints" => [
+                    new Length(["min" => 1, "max" => 256]),
+                    new NotBlank()
+                ],
+                "attr" => ["placeholder" => "Nombre del Restaurante"]
+            ])
+            ->add("Valoracion", IntegerType::class, [
+                "constraints" => [
+                    new Range(["min" => 1, "max" => 10]),
+                    new NotBlank()
+                ],
+                "attr" => ["placeholder" => "10"]
+            ])
+            ->add("Comentario", TextType::class, [
+                "constraints" => [
+                    new Length(["min" => 1, "max" => 256]),
+                    new NotBlank()
+                ],
+                "attr" => ["placeholder" => "Escribe tu comentario"]
+            ])
+            ->add("Guardar", SubmitType::class)
+            ->getForm();
+
+        $formulario->handleRequest($request);
+
+        if ($formulario->isSubmitted() && $formulario->isValid()) {
+            $restauranteNombre = $formulario->get("Restaurante")->getData();
+            $restaurante = $restauranteRepo->findOneBy(["Nombre" => $restauranteNombre]);
+
+            if ($restaurante !== null) {
+                $visita->setRestaurante($restaurante);
+                $emi->flush();
+
+                return $this->redirectToRoute("mostrartodos_visita");
+            }
+
+            $this->addFlash("error", "Restaurante no encontrado");
+        }
+
+        return $this->render("visita/actualizarVisita.html.twig", [
+            "formulario" => $formulario->createView()
+        ]);
+    }
+
+    #[Route("/visita/{idVisita}", name: "eliminar_visita", methods: ["POST"])]
+    public function eliminarVisita(EntityManagerInterface $emi, VisitaRepository $repo, int $idVisita): Response
+    {
+        $visita = $repo->find($idVisita);
+
+        if ($visita) {
+            $emi->remove($visita);
+            $emi->flush();
+        }
+
+        return $this->redirectToRoute("mostrartodos_visita");
     }
 }
